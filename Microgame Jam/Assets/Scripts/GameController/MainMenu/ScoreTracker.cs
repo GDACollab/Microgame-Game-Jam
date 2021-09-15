2b3d23aaccd39c09dabc4bdd8dcbba9b888d567d
ambiguousname <tyly04@gmail.com>
2021-09-14 21:31:31 -0700

    Added controller for lives

--- Microgame Jam/Assets/Scripts/GameController/MainMenu/ScoreTracker.cs
+++ Microgame Jam/Assets/Scripts/GameController/MainMenu/ScoreTracker.cs
@@ -9,11 +9,24 @@ public class ScoreTracker : MonoBehaviour
     public GameObject WinState;
     public GameObject LoseState;
 
+    public List<GameObject> liveObjects;
+
     private void OnEnable()
     {
         // This is to prevent any one animation from starting when TransitionScene is reloaded.
+        // Since these objects are not root level, their active states are saved, so we have to specifically make sure
+        // that they're set to be active.
         WinState.SetActive(false);
         LoseState.SetActive(false);
+
+        int lives = GameController.Instance.maxFails - GameController.Instance.gameFails;
+        foreach (GameObject life in liveObjects) {
+            // Iterate through all the displays of lives, set the ones that are unused to be not active.
+            if (lives <= 0) {
+                life.SetActive(false);
+            }
+            lives--;
+        }
     }
 
     public void DidWin(bool win, UnityEvent canHideGame, UnityEvent canShowGame, UnityEvent canUnpause) {
