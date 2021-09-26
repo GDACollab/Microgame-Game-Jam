952ee162e0565796f94c7533be7bdb2c414a7f13
ambiguousname <tyly04@gmail.com>
2021-09-26 16:54:45 -0700

    Updated package

--- Microgame Jam/Assets/Scripts/DDDDScripts/DontDropThatDungDung_Generator.cs
+++ Microgame Jam/Assets/Scripts/DDDDScripts/DontDropThatDungDung_Generator.cs
@@ -5,12 +5,13 @@ using UnityEngine;
 public class DontDropThatDungDung_Generator : MonoBehaviour
 {
     public GameObject[] layout_prefabs;
-
+    public GameObject DDDDobjects;
 
     void Start()
     {
         int layout = Random.Range(0,3);
-        Instantiate(layout_prefabs[layout]);
+        GameObject LayingOut = Instantiate(layout_prefabs[layout]) as GameObject;
+        LayingOut.transform.parent = DDDDobjects.transform;
     }
 
     
