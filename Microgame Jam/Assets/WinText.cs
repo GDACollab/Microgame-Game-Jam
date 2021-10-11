e7988b9120d16215789f711c128411561b8b1a10
ambiguousname <tyly04@gmail.com>
2021-10-11 10:55:05 -0700

    Added win count text

---
+++ Microgame Jam/Assets/WinText.cs
@@ -0,0 +1,40 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WinText : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void OnEnable()
+    {
+        var wins = GameController.Instance.gameWins;
+        var text = "You won " + wins + " times!";
+        if (wins >= 10)
+        {
+            text += "\nWow!";
+        }
+        else if (wins >= 20)
+        {
+            text += "\nAmazing!";
+        }
+        else if (wins >= 30)
+        {
+            text += "\nFantastic!";
+        }
+        else if (wins >= 50)
+        {
+            text += "\nI don't believe it!";
+        }
+        else if (wins >= 70)
+        {
+            text += "\nI don't believe it! No, seriously. I don't believe it. What did you <b>do</b>?";
+        }
+        else if (wins >= 100)
+        {
+            text += "\nLet's call this a win for everyone. Now get out of here!";
+        }
+        GetComponent<Text>().text = text;
+        transform.GetChild(0).GetComponent<Text>().text = text;
+    }
+}
